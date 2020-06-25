8eb1620a6f0a3da8aae8d8e65aef2f9e38457235
Michael Spikes <michael.spikes85@gmail.com>
2020-06-25 15:30:03 -0400

    Change theme from employees to something more fun. First menu is set up and view all is working.

--- index.js
+++ index.js
@@ -1,42 +1,124 @@
 const mysql = require("mysql");
-const inquirer = require("inquirer"),
-// const cTable = require("console.table"),
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
+  
 
- connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
 
     // Port
     port: 3306,
     user:"root",
     password:"root",
-    database: "company_db"
+    database: "gameofthrones_db"
 });
 
 // connection and error
 connection.connect(function(err)    {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    promptCompanyQuery();
+    console.log("Winter is coming!");
+    runSearch();
 });
 
 // CLI
-function promptCompanyQuery()    {
+function runSearch()    {
     inquirer.prompt({
         name: "action",
         type: "list",
-        message: "What do you want to do?",
+        message: "What would you like to do?",
         choices: [
-            "View employees",
-            "Add employee",
-            "Update employee",
-            "View departments",
-            "Add department",
-            "View roles",
-            "Add role",
+            "View all The Seven Kingdoms",
+            "View all by house",
+            "View all by loyalty",
+            "View all by position",
+            "Add person",
+            "Send person to their god/gods",
+            "Update person's position",
+            "Update person's loyalty",
+            "Update person's house",
+            "Add position",
+            "Remove position",
+            "Add house",
+            "Remove house",
+            "Add loyalty",
+            "Remove loyalty",
             "Quit",
 
         ]
     })
+    .then(function(answer) {
+        switch (answer.action) {
+        case "View all The Seven Kingdoms":
+          personSearch();
+          break;
+  
+        case "View all by house":
+          houseSearch();
+          break;
+  
+        case "View all by loyalty":
+          loyaltySearch();
+          break;
+  
+        case "View all by position":
+          positionSearch();
+          break;
+  
+        case "Add person":
+          addPerson();
+          break;
+          
+          case "Send person to their god/gods":
+          deadPerson();
+          break;
+
+          case "Update person's position":
+          updatePerson();
+          break;
+
+          case "Update person's loyalty":
+          updateLoyalty();
+          break;
+
+          case "Update person's house":
+          updateHouse();
+          break;
+
+          case "Add position":
+          addPosition();
+          break;
+
+          case "Remove position":
+          removePosition();
+          break;
+
+          case "Add house":
+          addHouse();
+          break;
+
+          case "Remove house":
+          removeHouse();
+          break;
+
+          case "Add loyalty":
+          addLoyalty();
+          break;
+          
+          case "Remove loyalty":
+          removeLoyalty();
+          break;
+
+          default: throw err;
+        }
+      });
 }
 
+function personSearch() {
+    let query = "SELECT * FROM person";
+    connection.query(query, function(err, res) {
+      if (err) throw err;
+      console.table(res);
+      runSearch();
+    });
+  }
